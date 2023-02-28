7b1a8c12b58e7419efe34d1e7d4b8361b375b2c5
Himanshu Yadav <97745121+Himanshuhi@users.noreply.github.com>
2023-02-28 21:33:34 +0530

    Update Rock-Paper-Scissors.py

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,9 +1,7 @@
 import random
 
-# Define the options
 options = ['rock', 'paper', 'scissors']
 
-# Define a function to determine the winner
 def determine_winner(player1, player2):
     if player1 == player2:
         return 'tie'
@@ -16,22 +14,17 @@ def determine_winner(player1, player2):
     else:
         return 'player2'
 
-# Main game loop
 while True:
-    # Get input from the players
     player1_choice = input("Player 1, choose rock, paper, or scissors: ").lower()
     while player1_choice not in options:
         player1_choice = input("Invalid input. Please choose rock, paper, or scissors: ").lower()
     player2_choice = random.choice(options)
 
-    # Print the choices
     print("Player 1 chose:", player1_choice)
     print("Himanshu chose:rock", player2_choice)
 
-    # Determine the winner
     winner = determine_winner(player1_choice, player2_choice)
 
-    # Print the result
     if winner == 'tie':
         print("It's a tie! Let's play again.")
     else:
